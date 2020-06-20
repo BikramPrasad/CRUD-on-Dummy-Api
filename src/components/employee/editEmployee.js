8c8c8fa65a92df7e1368f421e9a417a1a291487d
Bikram Prasad <prasadbikram97@gmail.com>
2020-06-20 14:57:25 +0530

    Update editEmployee.js
    chnages to editEmployee

--- src/components/employee/editEmployee.js
+++ src/components/employee/editEmployee.js
@@ -3,16 +3,67 @@ import { connect } from "react-redux";
 import { editEmployee } from "../../store/action/actions";
 
 class EditEmployee extends Component {
-  render() {
+  state = {
+    employee_name: "",
+    employee_salary: "",
+    employee_age: "",
+    profile_image: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.editEmployee(this.props.match.params.id, this.state);
+    this.props.history.push("/");
+  };
+  render(props) {
+    console.log(this.props.employees);
+    const employee = this.props.employees.find((emp) => emp.id === this.props.match.params.id);
     return (
-      <div>
+      <div className="container">
         <h2>Edit Employee</h2>
+        <form onSubmit={this.handleSubmit} className="white">
+          <div className="input-field">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="employee_name"
+              onChange={this.handleChange}
+              defaultValue={employee.employee_name}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="age">Age</label>
+            <input
+              type="text"
+              id="employee_age"
+              onChange={this.handleChange}
+              defaultValue={employee.employee_age}
+            />
+          </div>
+          <div className="input-field">
+            <label htmlFor="name">Salary</label>
+            <input
+              type="text"
+              id="employee_salary"
+              onChange={this.handleChange}
+              defaultValue={employee.employee_salary}
+            />
+          </div>
+          <div className="input-field">
+            <button className="btn waves-effect blue lighten-1 z-depth-0">Update</button>
+          </div>
+        </form>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
+  // console.log();
   return {
     employees: state.employees,
   };
@@ -20,8 +71,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    deleteEmployee: (empId, newEmp) => {
-      dispatch(editEmployee(empId, newEmp));
+    editEmployee: (empId, newEmpObj) => {
+      dispatch(editEmployee(empId, newEmpObj));
     },
   };
 };
